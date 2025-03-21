4116f830ec05e754e6750686ecc94fea8ef3237f
nidanaeem927 <naeemnida927@gmail.com>
2025-03-20 23:40:29 -0500

    feat: add AWS S3 bucket resource for credential validation

--- main.tf
+++ main.tf
@@ -1,14 +1,29 @@
 terraform {
   required_providers {
-    random = { source = "hashicorp/random", version = "3.5.1" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.0"  # Use a valid version
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
   }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "my-bucket-${random_string.demo.result}"
+}
+
 resource "random_string" "demo" {
-  length  = 10  
+  length  = 8
   special = false
 }
 
-output "result" {
-  value = random_string.demo.result
+output "bucket_name" {
+  value = aws_s3_bucket.demo.bucket
 }
